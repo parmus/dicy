e0a94ca92ea6dc3a6fdf417e6d83eff2e6c032b3
Martin Parm <parmus@gmail.com>
2018-02-24 22:28:52 +0100

    Better Door and Wall placement + debugging gizmos

--- Assets/Scripts/Tiles/ButtonTile.cs
+++ Assets/Scripts/Tiles/ButtonTile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[SelectionBase]
 public class ButtonTile : Tile {
 	[SerializeField] CubeSideType TriggerCubeSideType;
 	[SerializeField] Triggerable Triggerable;
@@ -10,5 +11,21 @@ public class ButtonTile : Tile {
 		if (TriggerCubeSideType == cubeSideType) {
 			Triggerable.Trigger();
     }
+	}
+
+	void OnDrawGizmos() {
+		if (TriggerCubeSideType == null || Triggerable == null) {
+			Gizmos.color = new Color(1f, 0f, 0f, 0.3f);
+			Gizmos.DrawCube(transform.position, new Vector3(1f,1f,1f));
+		}
+	}
+
+	void OnDrawGizmosSelected() {
+		if (Triggerable) {
+			Gizmos.color = new Color(0f, 1f, 0f, 0.3f);
+			Gizmos.DrawCube(Triggerable.transform.position, new Vector3(1f,1f,1f));
+			Gizmos.DrawRay(Triggerable.transform.position, Vector3.up);
+		}
+
 	}
 }
