52f4afb5ee9eda6dc54de6eb6769077b6e0451be
Martin Parm <parmus@gmail.com>
2018-02-25 05:10:03 +0100

    Rename DoorSound to SoundEffect

---
+++ Assets/Animations/Scripts/SoundEffect.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : StateMachineBehaviour {
+	[SerializeField] AudioClip AudioClip;
+
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		animator.gameObject.GetComponent<AudioSource>().PlayOneShot(AudioClip);	
+	}
+}
