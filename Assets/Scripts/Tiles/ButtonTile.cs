6d2a8abb0d34a506b7cf871885031ba834a21355
Martin Parm <parmus@gmail.com>
2018-02-25 17:38:15 +0100

    Allow ButtonTile to trigger multiple Triggerables

--- Assets/Scripts/Tiles/ButtonTile.cs
+++ Assets/Scripts/Tiles/ButtonTile.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 [SelectionBase]
 public class ButtonTile : Tile {
 	[SerializeField] CubeColor TriggerColor;
-	[SerializeField] Triggerable Triggerable;
+	[SerializeField] List<Triggerable> Triggerables = new List<Triggerable>();
 	[SerializeField] MeshRenderer Indicator;
 
 	[Header("Sounds")]
@@ -19,24 +20,26 @@ public class ButtonTile : Tile {
 	public override void Enter(Player player) {
 		if (player.BottomColor == TriggerColor) {
 			audioSource.PlayOneShot(TriggerSound);
-			Triggerable.Trigger();
+			foreach(var triggerable in Triggerables) {
+				if (triggerable) triggerable.Trigger();
+			}
     	} else {
 			audioSource.PlayOneShot(WrongColorSound);
 		}
 	}
 
 	void OnDrawGizmos() {
-		if (TriggerColor == null || Triggerable == null) {
+		if (TriggerColor == null || Triggerables.Count == 0 || Triggerables.Exists(t => t == null)) {
 			Gizmos.color = new Color(1f, 0f, 0f, 0.3f);
 			Gizmos.DrawCube(transform.position, new Vector3(1f,1f,1f));
 		}
 	}
 
 	void OnDrawGizmosSelected() {
-		if (Triggerable) {
-			Gizmos.color = new Color(0f, 1f, 0f, 0.3f);
-			Gizmos.DrawCube(Triggerable.transform.position, new Vector3(1f,1f,1f));
-			Gizmos.DrawRay(Triggerable.transform.position, Vector3.up);
+		Gizmos.color = new Color(0f, 1f, 0f, 0.3f);
+		foreach(var triggerable in Triggerables) {
+			if (triggerable == null) continue;
+			Gizmos.DrawCube(triggerable.transform.position, new Vector3(1f,1f,1f));
 		}
 	}
 
