e810576cf53fa44fce37c76a7a30c986f433c3cd
Martin Parm <parmus@gmail.com>
2018-02-25 16:09:41 +0100

    Persist music between levels

---
+++ Assets/Music.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+	private static Music instance = null;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad(this);
+		} else {
+			Destroy(this.gameObject);
+		}
+	}
+}
