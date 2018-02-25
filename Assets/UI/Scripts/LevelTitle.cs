8c092b38e09abb42bbd229295a12b8b902b8e629
Martin Parm <parmus@gmail.com>
2018-02-25 21:55:47 +0100

    Level Title UI

---
+++ Assets/UI/Scripts/LevelTitle.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelTitle : MonoBehaviour {
+	void Start () {
+		Text label = GetComponent<Text>();
+		label.text = SceneManager.GetActiveScene().name;
+		
+	}
+}
