e2c96de82d4e537a5f47a196500ed20380c14b74
Martin Parm <parmus@gmail.com>
2018-02-25 23:29:55 +0100

    Fix Basic Movement build

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -33,7 +33,6 @@ public class Player : MonoBehaviour {
     }
 
 	public void LevelComplete() {
-	    // TODO: Win animation?
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 
