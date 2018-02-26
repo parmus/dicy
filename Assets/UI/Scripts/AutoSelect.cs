86b4ba5289f16d14f6e8b1948caf69a2a5d12a3a
Martin Parm <parmus@gmail.com>
2018-02-27 00:27:52 +0100

    Pause Menu

---
+++ Assets/UI/Scripts/AutoSelect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class AutoSelect : MonoBehaviour {
+	void OnEnable() {
+		StartCoroutine(selectFirst());
+	}
+
+	private IEnumerator selectFirst() {
+		Selectable firstSelectable = GetComponentInChildren<Selectable>();
+		if (firstSelectable) {
+			EventSystem.current.SetSelectedGameObject(null);
+			yield return null;
+			firstSelectable.Select();
+		}
+	}
+}
---
+++ Assets/UI/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+    [SerializeField] GameObject PauseMenuUI;
+
+    public static bool IsPaused { get { return paused; } }
+
+    private static bool paused = false;
+
+    void Start () {
+		Resume();		
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (paused) {
+				Resume();
+			} else {
+				Pause();				
+			}
+		}		
+	}
+
+	public void Pause() {
+		Time.timeScale = 0;
+		paused = true;
+		PauseMenuUI.SetActive(true);
+	}
+
+	public void Resume() {
+		Time.timeScale = 1f;
+		paused = false;
+		PauseMenuUI.SetActive(false);
+	}
+
+	public void MainMenu() {
+		Resume();
+		SceneManager.LoadScene(0);
+	}
+
+	public void RestartLevel() {
+		Resume();
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
