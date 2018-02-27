d01a887ad4ddbecdc2f14aea8c881bfa695e534a
Martin Parm <parmus@gmail.com>
2018-02-27 02:00:17 +0100

    Updated Level Title to TMPro

--- Assets/UI/Scripts/LevelTitle.cs
+++ Assets/UI/Scripts/LevelTitle.cs
@@ -4,8 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class LevelTitle : MonoBehaviour {
 	void Start () {
-		Text label = GetComponent<Text>();
-		label.text = SceneManager.GetActiveScene().name;
+		TMPro.TMP_Text label = GetComponent<TMPro.TMP_Text>();
+		label.SetText(SceneManager.GetActiveScene().name);
 		
 	}
 }
