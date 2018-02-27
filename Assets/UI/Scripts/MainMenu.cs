7ff3566c4796740a7aaa361cc98c14d1a9a2a3ab
Martin Parm <parmus@gmail.com>
2018-02-27 01:37:41 +0100

    Allow quit to work within the editor

--- Assets/UI/Scripts/MainMenu.cs
+++ Assets/UI/Scripts/MainMenu.cs
@@ -8,6 +8,10 @@ public class MainMenu : MonoBehaviour {
 	}
 
 	public void Quit() {
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
 		Application.Quit();
+#endif
 	}
 }
