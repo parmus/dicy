4c4c03f95b3df11731d5a0c0a4508816c8cbf8b6
Martin Parm <parmus@gmail.com>
2018-03-01 00:14:19 +0100

    Allow bridges to start open

--- Assets/Scripts/Tiles/BridgeTile.cs
+++ Assets/Scripts/Tiles/BridgeTile.cs
@@ -2,15 +2,23 @@
 
 [SelectionBase]
 public class BridgeTile : Triggerable {
-	private bool isOpen = true;
+	[SerializeField] bool startOpen = true;
+
 	private Animator animator;
 
   void Start () {
 		animator = GetComponent<Animator>();
+		animator.SetBool("startOpen", startOpen);
 	}
 
   public override void Trigger() {
-		isOpen = !isOpen;
-		animator.SetBool("isOpen", isOpen);
-    }
+		animator.SetTrigger("Toggle");
+	}
+
+	void OnDrawGizmos() {
+		if (!startOpen) {
+			Gizmos.color = new Color(0, 1, 0, 0.3f);
+			Gizmos.DrawCube(transform.position + Vector3.up * 0.45f, new Vector3(0.99f, 0.1f, 0.99f));
+		}
+	}
 }
