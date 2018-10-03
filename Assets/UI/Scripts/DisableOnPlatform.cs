d43e8a3e09a20ad25f3da9bf106b0ec73f40ac48
Martin Parm <parmus@gmail.com>
2018-10-03 17:46:56 +0200

    WebGL improvements
    - Disable quit button on WebGL
    - Build with minimal frame

---
+++ Assets/UI/Scripts/DisableOnPlatform.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnPlatform : MonoBehaviour {
+	[SerializeField] RuntimePlatform Platform;
+
+	void OnEnable() {
+		if (Application.platform == Platform) {
+			gameObject.SetActive(false);
+		}
+	}
+}
