f8426d229332be6e9496e1d349e99efe8e424b6a
Martin Parm <parmus@gmail.com>
2018-10-03 23:31:56 +0200

    Break Fragile Tile using physics
    - Remove hardcoded animation
    - Add physics
    - Add particles
    - Add sound effects

--- Assets/Scripts/Tiles/FragileTile.cs
+++ Assets/Scripts/Tiles/FragileTile.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FragileTile : Tile {
-  private Animator animator;
+	[Header("Sound Effects")]
+	[SerializeField] AudioClip CrumbleSound;
+	[SerializeField] AudioClip BreakSound;
+	[SerializeField] List<Rigidbody> CrumblingParts = new List<Rigidbody>();
+	[SerializeField] List<Rigidbody> BreakingParts = new List<Rigidbody>();
 
-  void Start () {
-	animator = GetComponent<Animator>();
-  }
+	new private Collider collider;
+	private AudioSource audioSource;
+	private ParticleSystem crumbles;
 
-  override public void Leave(Player player) {
-	  animator.SetTrigger("Break");
-  }
+	void Start () {
+		collider = GetComponent<Collider>();
+		audioSource = GetComponent<AudioSource>();
+		crumbles = GetComponentInChildren<ParticleSystem>();
+	}
+
+	override public void Enter(Player player) {
+		audioSource.PlayOneShot(CrumbleSound);
+		crumbles.Play();
+		foreach(Rigidbody part in CrumblingParts) {
+			part.isKinematic = false;
+			part.AddTorque(Random.Range(-100f, 100f), 0f, Random.Range(-100f, 100f));
+			Destroy(part.gameObject, 5f);
+		}
+	}
+
+	override public void Leave(Player player) {
+		audioSource.PlayOneShot(BreakSound);
+		collider.enabled = false;
+		foreach(Rigidbody part in BreakingParts) {
+			part.isKinematic = false;
+			part.AddTorque(Random.Range(-100f, 100f), 0f, Random.Range(-100f, 100f));
+			Destroy(part.gameObject, 5f);
+		}
+	}
 }
