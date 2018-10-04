168fd1cda0b292ddba0791d81f2da1b2845264b3
Martin Parm <parmus@gmail.com>
2018-10-04 19:29:50 +0200

    Add end credits

---
+++ Assets/UI/Scripts/EndCredits.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EndCredits : MonoBehaviour {
+	[SerializeField] float ScrollSpeed = 50f;
+	[SerializeField] float StartOffset = 25f;
+	[SerializeField] SceneFader SceneFader;
+
+	private RectTransform rectTransform;
+
+	private float position;
+	private float height;
+
+	void Start() {
+		rectTransform = GetComponent<RectTransform>();
+
+		float parentHeight = transform.parent.GetComponent<RectTransform>().rect.height;
+		position = -(parentHeight + StartOffset);
+		height = rectTransform.rect.height;
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			SceneFader.LoadMainMenu();
+		}
+
+		scrollCredits();
+	}
+
+	private void scrollCredits() {
+		if (position > height) mainMenu();
+
+		position += ScrollSpeed * Time.deltaTime;
+
+		Vector2 offsetMax = rectTransform.offsetMax;
+		offsetMax.y = position;
+		rectTransform.offsetMax = offsetMax;
+
+		Vector2 offsetMin = rectTransform.offsetMin;
+		offsetMin.y = offsetMax.y - height;
+		rectTransform.offsetMin = offsetMin;
+	}
+
+	private void mainMenu() {
+		SceneFader.LoadMainMenu();
+	}
+}
---
+++ Assets/UI/Scripts/SceneFader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour {
+	private CanvasGroup canvasGroup;
+
+	void Start () {
+		canvasGroup = GetComponentInChildren<CanvasGroup>();
+
+		canvasGroup.alpha = 1f;
+		StartCoroutine(fadeIn());
+	}
+
+	public void LoadMainMenu() {
+		LoadScene(0);
+	}
+
+	public void ReloadScene() {
+		LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void LoadNextScene() {
+		LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void LoadScene(int sceneId) {
+		StartCoroutine(fadeToLevel(sceneId));
+	}
+
+	private IEnumerator fadeToLevel(int sceneId) {
+		while(canvasGroup.alpha < 1.0f) {
+			canvasGroup.alpha += Time.deltaTime;
+			yield return new WaitForFixedUpdate();
+		}
+
+		SceneManager.LoadScene(sceneId);
+	}
+
+	private IEnumerator fadeIn() {
+		while(canvasGroup.alpha > 0.0f) {
+			canvasGroup.alpha -= Time.deltaTime;
+			yield return new WaitForFixedUpdate();
+		}
+	}
+}
